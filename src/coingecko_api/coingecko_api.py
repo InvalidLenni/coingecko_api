d0b7047749634828b64281816c04e9a5901dfdde
ran <ran-n@tutanota.com>
2021-10-31 19:06:34 +0100

    cambiadas tabulacions

--- src/coingecko_api/coingecko_api.py
+++ src/coingecko_api/coingecko_api.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2021/10/24 18:10:22.139504
-#+ Editado:	2021/10/31 18:16:57.453180
+#+ Editado:	2021/10/31 19:06:05.307838
 # ------------------------------------------------------------------------------
 import requests as r
 import json
@@ -31,7 +31,7 @@ class CoinGecko:
     # Operacións ---------------------------------------------------------------
     # /ping
     def ping(self) -> dict:
-    """
+        """
         Pingea á api para ver se está disponhible.
 
         @entrada:
@@ -40,12 +40,12 @@ class CoinGecko:
         @saída:
             Dicionario -   Sempre
             └ Chave "gecko_says" e contido "(V3) To the Moon!".
-    """
+        """
         return json.loads(r.get(self.get_url_base()+'ping').text)
 
     # /coins
     def get_coins(self) -> List[dict]:
-    """
+        """
         Lista de moedas composta por dicionarios co id, símbolo, nome, imaxes, tempo
         bloques en minutos e informacións de mercado e prezo varias.
         Ordeada por ranking (#).
@@ -56,12 +56,12 @@ class CoinGecko:
         @saída:
             Lista de dicionarios   -   Sempre
             └ Todas as moedas de CoinGecko.
-    """
+        """
         return json.loads(r.get(self.get_url_base()+'coins').text)
 
     # /coins/{id}
     def get_coin(self, id_moeda: str) -> dict:
-    """
+        """
         Devolve unha gran cantidade de información sobre unha moeda concreta.
 
         @entrada:
@@ -72,12 +72,12 @@ class CoinGecko:
             Dicionario  -   Sempre
             └ Con toda a información sobre esa moeda ou co erro coa chave "error"
             e de contido unha mensaxe explicando que o id non foi atopado.
-    """
+        """
         return json.loads(r.get(self.get_url_base()+'coins/'+id_moeda).text)
 
     # /coins/list
     def get_coins_list(self) -> List[dict]:
-    """
+        """
         Lista de moedas composta por dicionarios co id, símbolo e nome.
         Ordeada por id.
 
@@ -86,7 +86,7 @@ class CoinGecko:
         @saída:
             Lista de dicionarios   -   Sempre
             └ Todas as moedas de CoinGecko.
-    """
+        """
         return json.loads(r.get(self.get_url_base()+'coins/list').text)
     # --------------------------------------------------------------------------
 
@@ -96,6 +96,7 @@ def main():
     #print(cg.ping())
     #print(cg.get_coins()[0]['id'])
     #print(cg.get_coins_list()[1329])
+    #print(cg.get_coin('bitcoin'))
 
 if __name__=='__main__':
     main()
