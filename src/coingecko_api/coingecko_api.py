6ea53d3683d5bd48aa3970341612e08083fca94a
ran <ran-n@tutanota.com>
2021-10-31 17:36:05 +0100

    +Comentarios das funcions feitas

--- src/coingecko_api/coingecko_api.py
+++ src/coingecko_api/coingecko_api.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2021/10/24 18:10:22.139504
-#+ Editado:	2021/10/28 15:07:29.434136
+#+ Editado:	2021/10/31 17:35:32.373654
 # ------------------------------------------------------------------------------
 import requests
 import json
@@ -30,14 +30,46 @@ class CoinGecko:
     # Operacións ---------------------------------------------------------------
     # /ping
     def ping(self):
+        """
+        Pingea á api para ver se está disponhible.
+
+        @entrada:
+            Ningunha.
+
+        @saída:
+            Dicionario -   Sempre
+            └ Chave "gecko_says" e contido "(V3) To the Moon!".
+        """
         return json.loads(requests.get(self.get_url_base()+'ping').text)
 
     # /coins
     def get_coins(self):
+        """
+        Lista de moedas composta por dicionarios co id, símbolo, nome, imaxes, tempo
+        bloques en minutos e informacións de mercado e prezo varias.
+        Ordeada por ranking (#).
+
+        @entrada:
+            Ningunha.
+
+        @saída:
+            Lista de dicionarios   -   Sempre
+            └ Todas as moedas de CoinGecko.
+        """
         return json.loads(requests.get(self.get_url_base()+'coins').text)
 
     # /coins/list
     def get_coins_list(self):
+        """
+        Lista de moedas composta por dicionarios co id, símbolo e nome.
+        Ordeada por id.
+
+        @entrada:
+
+        @saída:
+            Lista de dicionarios   -   Sempre
+            └ Todas as moedas de CoinGecko.
+        """
         return json.loads(requests.get(self.get_url_base()+'coins/list').text)
     # --------------------------------------------------------------------------
 
