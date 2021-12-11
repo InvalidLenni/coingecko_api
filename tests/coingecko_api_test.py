9214ddb3313615d76194fecb5e7873521f84a3b1
ran <ran-n@tutanota.com>
2021-12-11 23:16:39 +0100

    + novo test á metade

--- tests/coingecko_api_test.py
+++ tests/coingecko_api_test.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado: 	2021/12/09 22:13:41.735240
-#+ Editado:	2021/12/10 17:43:42.301951
+#+ Editado:	2021/12/11 21:51:50.660699
 # ------------------------------------------------------------------------------
 import requests as r
 import json
@@ -31,6 +31,18 @@ class TestCoinGecko_API(unittest.TestCase):
         cg = CoinGecko()
 
         self.assertEqual(cg.ping(), self.get(url))
+
+    def test_get_price(self):
+        probas_ids = ['bitcoin', ['bitcoin', 'ethereum']]
+        probas_vs_currencies = ['eur', ['eur', 'usd']]
+        cg = CoinGecko()
+
+        url = f'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=eur,usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false'
+        self.assertEqual(cg.get_price(['bitcoin', 'ethereum'], ['eur', 'usd']), self.get(url))
+        #for identificador, vs_currencies in zip(probas_ids, probas_vs_currencies):
+            #url = f'https://api.coingecko.com/api/v3/simple/price?ids={identificador}&vs_currencies={vs_currencies}&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false'
+
+            #self.assertEqual(cg.get_price(identificador, vs_currencies), self.get(url))
 
 # ------------------------------------------------------------------------------
 
